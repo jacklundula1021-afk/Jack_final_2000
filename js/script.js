94057b83029b4e0cb359fa89eb84754f4013b1a0
jackarta <jacklundula1021@gmail.com>
2026-06-29 04:27:05 -0600

    done with page about

--- js/script.js
+++ js/script.js
@@ -11,3 +11,27 @@ window.addEventListener("resize", () => {
         menu.classList.remove("active");
     }
 });
+
+
+
+//Show/hide content on button click
+
+
+const buttons = document.querySelectorAll(".about-content-btn");
+
+buttons.forEach(btn => {
+    btn.addEventListener("click", () => {
+
+        const content = btn.nextElementSibling;
+
+        // close all
+        document.querySelectorAll(".about-hidden-content").forEach(c => {
+            if (c !== content) c.style.display = "none";
+        });
+
+        // toggle current
+        content.style.display =
+            content.style.display === "block" ? "none" : "block";
+
+    });
+});
